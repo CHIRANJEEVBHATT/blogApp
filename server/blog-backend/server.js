703d5f3979ce2783c86b81929fb88cca52724e45
Chiranjeev Bhatt <chiranjeevbhatt18@gmail.com>
2025-09-07 22:20:31 +0530

    fix: deploymennt

--- server/blog-backend/server.js
+++ server/blog-backend/server.js
@@ -20,7 +20,7 @@ app.use(cors(
   {
     origin: [
       'https://blog-app-sigma-gold.vercel.app',
-      'http://localhost:3000',
+      'http://localhost:4000',
       'http://localhost:5173'
     ],
     credentials: true
